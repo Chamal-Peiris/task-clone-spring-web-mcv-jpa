4b1093aafa32f3eafce785d41f575d022c3a2b41
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-06-17 18:49:07 +0530

    Add the Appinitializer,Appconfig classes to intialize the AnnotationApplicationContext

---
+++ src/main/java/lk/ijse/dep8/tasks/config/AppConfig.java
@@ -0,0 +1,10 @@
+package lk.ijse.dep8.tasks.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@ComponentScan("lk.ijse.dep8.tasks")
+@Configuration
+public class AppConfig {
+}
