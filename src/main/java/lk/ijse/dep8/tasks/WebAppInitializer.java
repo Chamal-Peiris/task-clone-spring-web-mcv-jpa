3bf4e2673ea0e25bc4c47c789e68f10629189133
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-06-22 19:04:51 +0530

    Add WebAppInitializer class

---
+++ src/main/java/lk/ijse/dep8/tasks/WebAppInitializer.java
@@ -0,0 +1,6 @@
+package lk.ijse.dep8.tasks;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+}
